89b40374fffa90f4165de7fc05722cc97e797011
Ian Thompson <ianthompson@nicelion.com>
2018-03-19 18:58:29 -0400

    fished Flag and everything else

--- StarPatterns.java
+++ StarPatterns.java
@@ -9,7 +9,6 @@
  * @version Today
  */
 
-
 class StarPatterns 
 {
     public static void starGrid(int h, int w)
@@ -23,7 +22,7 @@ class StarPatterns
             System.out.println();
         }   
     }
-    
+
     public static void starBox(int h, int w)
     {
         for (int i=0; i<h; i++)
@@ -53,7 +52,7 @@ class StarPatterns
             System.out.println();
         }   
     }
-    
+
     public static void starZ(int h)
     {
         int w = h;
@@ -72,10 +71,10 @@ class StarPatterns
             System.out.println();
         }
     }
-    
+
     public static void starBox(int h) {
         int w = h;
-        
+
         for (int i=0; i<h; i++)
         {
             for (int j=0; j<w; j++)
@@ -87,11 +86,11 @@ class StarPatterns
             System.out.println();
         }   
     }
-    
+
     public static void twoStarBoxes(int h, int w) {
         int hH = h/2;
         int wH = h/2;
-        
+
         for (int i=0; i<h; i++) {
             for (int j=0; j<w; j++) {               
                 if((i<hH && j<wH)||(i>=hH && j>=wH))System.out.print("*");
@@ -100,7 +99,7 @@ class StarPatterns
             System.out.println();
         }      
     }
-    
+
     public static void starTriangle(int h, int w) {
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
@@ -109,11 +108,11 @@ class StarPatterns
             }
             System.out.println();
         }
-        
+
     }
-    
+
     public static void emptyTriangle(int h, int w) {
-    
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 if (i == j || i == h - 1 || j == 0) System.out.print("*");
@@ -122,7 +121,7 @@ class StarPatterns
             System.out.println();
         }
     }
-    
+
     public static void starTriangleUR(int h, int w) {
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
@@ -132,10 +131,10 @@ class StarPatterns
             System.out.println();
         }
     }
-    
+
     public static void isoscelesStarTriangle(int h, int w) {
         w = 2 * w - 1;
-        
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 if (Math.abs(j - (w / 2)) < i) System.out.print("*");
@@ -143,9 +142,9 @@ class StarPatterns
             }
             System.out.println();
         }
-        
+
     }
-    
+
     public static void checkerBoard(int h, int w) {
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
@@ -155,7 +154,7 @@ class StarPatterns
             System.out.println();
         }
     }
-    
+
     public static void biggerCheckerBoard(int w, int h, int size) {
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
@@ -165,6 +164,52 @@ class StarPatterns
             System.out.println();
         }
     }
+
+    public static void upsideDownCheckeredTriangle(int h, int w) {
+        w = 2 * w - 1;
+        int count = w / 2;
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if ((j >= (w / 2) - count && j <= (w / 2) + count) && Math.abs(j-count) % 2 != 0) System.out.print("*");
+                else System.out.print(" ");
+            }
+            count--;
+            System.out.println();
+        }
+    }
+
+    public static void primeStars(int h) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < nthPrime(i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void fibonacciStars(int h) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < fibonacci(i); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void starFlag(int w, int h) {
+        
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (j <= 15 && i < 6) System.out.print("*");
+                else if ((j > 15 || i >= 5) && i % 2 == 0) System.out.print("R");
+                else System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    
+    }
     
     public static void main(String[] args) 
     {
@@ -173,36 +218,79 @@ class StarPatterns
 
         starBox(8, 8);
         System.out.println();
-        
+
         starX(8);
         System.out.println();
-        
+
         starZ(8);
         System.out.println();
-        
+
         starBox(8);
         System.out.println();
-        
+
         twoStarBoxes(8, 8);
         System.out.println();
-        
+
         starTriangle(8, 8);
         System.out.println();
-        
+
         emptyTriangle(8, 8);
         System.out.println();
-        
+
         starTriangleUR(8, 8);
         System.out.println();
-        
+
         isoscelesStarTriangle(8, 8);
         System.out.println();
-            
+
         checkerBoard(8, 8);
         System.out.println();
-        
+
         biggerCheckerBoard(30, 20, 5);
         System.out.println();
+
+        upsideDownCheckeredTriangle(8, 8);
+        System.out.println();
+        
+        primeStars(8);
+        System.out.println();
+        
+        fibonacciStars(10);
+        System.out.println();
+        
+        starFlag(35, 20);
+        System.out.println();
     }
-     
+
+    /** Helper Functions **/
+    static public int nthPrime(int n) {
+        int i = 0;
+        int count = 0;
+
+        while (i < n) {
+            count++;
+            if (isPrime(count)) i++;
+        }
+
+        return count;
+    }
+
+    public static boolean isPrime(int n){
+        if (n == 2) return true;
+        else if (n % 2==0 || n < 2) return false;
+
+        double limit = Math.sqrt(n);
+
+        for (int i=3; i<=limit; i+=2)
+        {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
